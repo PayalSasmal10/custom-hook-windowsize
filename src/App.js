1e70212999e8bb4095e8caaf656c744897271a00
Payal Sasmal <sasmalpayal@gmail.com>
2023-06-17 12:36:48 +0530

    window size is completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useSize from "./UseSize";
 
 function App() {
+  const windowSize = useSize();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Width : <span>{windowSize[0]}</span>
+      </div>
+      <div>
+        Height : <span>{windowSize[1]}</span>
+      </div>
     </div>
   );
 }
